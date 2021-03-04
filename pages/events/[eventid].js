99f18a5012ef11dbced0cfc6783d754245b5983a
Vishesh Singh <VisheshSingh16@gmail.com>
2021-03-04 15:58:32 -0600

    Event Detail page

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,10 +1,30 @@
 import React from 'react';
+import EventLogistics from '../../components/events/EventLogistics';
+import EventSummary from '../../components/events/EventSummary';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventContent from '../../components/events/EventContent';
 
 const EventDetailsPage = () => {
+  const router = useRouter();
+  const eventId = router.query.eventid;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <h1>No event found!</h1>;
+  }
+  console.log(event);
   return (
-    <div>
-      <h1>Event details page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </>
   );
 };
 
