b6c7d87760975c9aee9905e64554df45d6b8d9f6
Vishesh Singh <VisheshSingh16@gmail.com>
2021-03-05 11:09:21 -0600

    SSG to all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../utils/api-utils';
 import EventList from '../../components/events/EventList';
+import { useRouter } from 'next/router';
+import EventFilterForm from '../../components/events/EventFilterForm';
 
-const AllEventsPage = () => {
-  const events = getAllEvents();
+const AllEventsPage = (props) => {
+  const router = useRouter();
 
-  return <EventList items={events} />;
+  const filterHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+  return (
+    <>
+      <EventFilterForm onSearch={filterHandler} />
+      <EventList items={props.events} />
+    </>
+  );
 };
 
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 1800,
+  };
+}
 export default AllEventsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
-import EventFilterForm from '../components/events/EventFilterForm';
 import EventList from '../components/events/EventList';
 import { getFeaturedEvents } from '../utils/api-utils';
-import { useRouter } from 'next/router';
 
 export default function Home(props) {
-  const router = useRouter();
-
-  const filterHandler = (year, month) => {
-    const fullPath = `/events/${year}/${month}`;
-    router.push(fullPath);
-  };
-
   return (
     <>
-      <EventFilterForm onSearch={filterHandler} />
       <EventList items={props.events} />
     </>
   );
