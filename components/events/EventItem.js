6b94275266277112e26f214842196babb889edf5
Vishesh Singh <VisheshSingh16@gmail.com>
2021-03-05 14:35:26 -0600

    Image Component 🖼

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -4,6 +4,7 @@ import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
 import Button from '../ui/button';
+import Image from 'next/image';
 
 const EventItem = (props) => {
   const { id, title, date, location, image } = props;
@@ -17,7 +18,7 @@ const EventItem = (props) => {
 
   return (
     <li className={styles.item}>
-      <img src={`/${image}`} alt={`${title}`} />
+      <Image src={`/${image}`} alt={`${title}`} width={250} height={160} />
       <div className={styles.content}>
         <div className={styles.summary}>
           <h2>{title}</h2>
