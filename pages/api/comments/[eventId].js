2e30c46c919bdd48e09c769f30798fd9c044931d
Vishesh Singh <VisheshSingh16@gmail.com>
2021-03-05 19:36:59 -0600

    connecting to MongoDB 🗄

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,10 @@
-export default (req, res) => {
+import { MongoClient } from 'mongodb';
+
+export default async (req, res) => {
   const eventId = req.query.eventId;
+  const client = await MongoClient.connect(
+    `mongodb+srv://test1234:test1234@cluster0.gvoli.mongodb.net/events?retryWrites=true&w=majority`
+  );
 
   if (req.method === 'POST') {
     const { email, name, text } = req.body;
@@ -13,25 +18,31 @@ export default (req, res) => {
     ) {
       res.status(422).json({ message: 'Invalid data provided!' });
     }
+
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId,
     };
+
+    const db = client.db();
+
+    const result = await db.collection('comments').insertOne(newComment);
+    // console.log(result);
+    newComment.id = result.insertedId;
     res.status(201).json({ message: 'Comment added', comment: newComment });
   }
 
   if (req.method === 'GET') {
-    const dummyComments = [
-      { id: 'c1', text: 'first comment', name: 'Max', email: 'max@gmail.com' },
-      {
-        id: 'c2',
-        text: 'second comment',
-        name: 'Manu',
-        email: 'manu@gmail.com',
-      },
-    ];
-    return res.status(200).json({ comments: dummyComments });
+    const db = client.db();
+
+    const documents = await db
+      .collection('comments')
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+    return res.status(200).json({ comments: documents });
   }
+  client.close();
 };
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,11 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { MongoClient } from 'mongodb';
 
-export default (req, res) => {
+export default async (req, res) => {
   if (req.method === 'POST') {
     const { email } = req.body;
     if (!email) {
       return res.status(422).json({ message: 'Invalid email!' });
     }
+
+    const client = await MongoClient.connect(
+      `mongodb+srv://test1234:test1234@cluster0.gvoli.mongodb.net/events?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+    await db.collection('newsletter').insertOne({ email });
+    client.close();
+
     res
       .status(201)
       .json({ message: 'Thanks for signing up to the newsletter!' });
